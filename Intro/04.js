5babecc05086641b916eb821e0dfc11780acbe57
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2017-12-26 10:41:09 -0400

    Create 04.js

---
+++ Intro/04.js
@@ -0,0 +1,9 @@
+function adjacentElementsProduct(inputArray) {
+    var max=inputArray[0]*inputArray[1];
+    for (var i=0;i<inputArray.length-1;i++){
+        if (inputArray[i]*inputArray[i+1]>max){
+            max=inputArray[i]*inputArray[i+1]
+        }
+    }
+    return max
+}

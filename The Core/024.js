212c679a8d7711423173c7ea2e3b560e5b1cc8fd
jesusdanielm58 <33935377+jesusdanielm58@users.noreply.github.com>
2018-01-06 08:00:57 -0400

    Create 024.js

---
+++ The Core/024.js
@@ -0,0 +1,18 @@
+function equalPairOfBits(n, m) {
+  return (function(){
+    n=n.toString(2);
+    m=m.toString(2);
+    min=Math.min(n.length,m.length)
+    max=Math.max(n.length,m.length)
+    if(n.length<m.length){n="0".repeat(max-min)+n}
+    else{m="0".repeat(max-min)+m}
+    m=m.split("").reverse();
+    n=n.split("").reverse();
+    found=false
+    for(var i=0;i<m.length;i++){
+      if(n[i]===m[i]){found=true;break}
+    }
+    if(found==false){i++}
+    return Math.pow(2,i)
+  })();
+}
